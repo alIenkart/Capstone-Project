653f06c2593f5e6879ef6ec86da23126fcba3db9
Cjeffrey125 <93568594+Cjeffrey125@users.noreply.github.com>
2025-05-23 20:06:12 +0800

    Added Laravel Breeze/Inertia

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+
+
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <p>Hello World</p>
+
+    <Link :href="route('login')">
+        Log in
+    </Link>
+
+    <Link :href="route('register')">
+        Register
+    </Link>
+</template>
