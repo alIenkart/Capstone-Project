458fc3829e040a3e9e45d05c57031f7394ffd38e
Cjeffrey125 <93568594+Cjeffrey125@users.noreply.github.com>
2025-06-18 20:32:03 +0800

    Added Router Validation

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -33,7 +33,12 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        $user = $request->user();
+        if ($user->role === 'Admin') {
+            return redirect()->route('admin.admindashboard');
+        }
+
+        return redirect()->route('dashboard');
     }
 
     /**
