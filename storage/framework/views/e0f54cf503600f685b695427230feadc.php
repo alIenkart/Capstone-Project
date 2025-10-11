3714b34a8d8653b8e802c37278c94fa5252ec5d4
Cjeffrey125 <93568594+Cjeffrey125@users.noreply.github.com>
2025-10-11 18:39:47 +0800

    Updated gitignore and added images

---
+++ storage/framework/views/e0f54cf503600f685b695427230feadc.php
@@ -0,0 +1,7 @@
+
+
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\Users\jason\Documents\Jefff\Pororoject\5-22-25\Here\Capstone-Project\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
