a2b33a35567362ae25c79410b3837ad72908afc7
Cjeffrey125 <93568594+Cjeffrey125@users.noreply.github.com>
2025-10-12 11:34:44 +0800

    Added customer_id on Payments table

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -9,6 +9,7 @@ class Payment extends Model
     protected $fillable = [
         'booking_id',
         'amount_paid',
+        'customer_id',
         'payment_date',
         'payment_status',
         'total_price',
@@ -27,6 +28,7 @@ class Payment extends Model
             [
                 'total_price' => $booking->total_price,
                 'amount_paid' => 0,
+                'customer_id' => $booking->customer_id,
                 'remaining_payment' => $booking->total_price,
                 'payment_status' => 'Pending',
                 'payment_date' => now(),
