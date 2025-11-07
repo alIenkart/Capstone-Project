2b549d379ef760d90d09e6b53e7747b47180d9ba
Marco-FE <163421468+JanMarco-Dev@users.noreply.github.com>
2025-11-07 20:57:57 +0800

    push update.

--- database/migrations/2025_05_23_131405_users_update_columns.php
+++ database/migrations/2025_05_23_131405_users_update_columns.php
@@ -10,10 +10,15 @@ return new class extends Migration
     {
 
         Schema::table('users', function (Blueprint $table) {
-            $table->renameColumn('name', 'first_name');
-            $table->string('last_name')->after('first_name');
-            $table->string('phone_number')->nullable()->after('email');
-            $table->string('role')->default('customer')->after('phone_number');
+            if (!Schema::hasColumn('users', 'last_name')) {
+                $table->string('last_name')->after('first_name');
+            }
+            if (!Schema::hasColumn('users', 'phone_number')) {
+                $table->string('phone_number')->nullable()->after('email');
+            }
+            if (!Schema::hasColumn('users', 'role')) {
+                $table->string('role')->default('customer')->after('phone_number');
+            }
         });
     }
     
