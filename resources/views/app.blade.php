a3ee643f2dbfb51bfb5bec60952731f6995bd965
Jeff-DEV <93568594+Cjeffrey125@users.noreply.github.com>
2025-11-10 23:02:33 +0800

    Added Logo and Website Name

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,12 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', "JE Travel & Tours") }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+        <link rel="icon" type="image/png" href="{{ asset('storage/logo/Logo.png') }}">
 
         <!-- Scripts -->
         @routes
